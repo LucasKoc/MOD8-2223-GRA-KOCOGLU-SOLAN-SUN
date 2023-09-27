41fabd2e14fcfdd83630cb06f2e44e431da1f9c2
Tao Solan <tao.solan@gmail.com>
2023-09-27 17:03:20 -0400

    Initializing Vue

---
+++ vue/src/App.vue
@@ -0,0 +1,54 @@
+<script>
+import LoginOverlay from './components/LoginOverlay.vue';
+
+export default {
+  data() {
+    return {
+      showLoginOverlay: false,
+    };
+  },
+  components: {
+    LoginOverlay,
+  },
+  methods: {
+    openLoginOverlay() {
+      this.showLoginOverlay = true;
+    },
+    closeLoginOverlay() {
+      this.showLoginOverlay = false;
+    },
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <div class="container">
+      <!-- Fancy homepage content -->
+      <h1>Welcome to Our Website</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit....</p>
+      <button @click="openLoginOverlay">Login</button>
+    </div>
+
+    <!-- Login overlay -->
+    <LoginOverlay v-if="showLoginOverlay" @close="closeLoginOverlay" />
+  </div>
+</template>
+
+<style>
+
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ vue/src/components/LoginOverlay.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="overlay">
+      <div class="login-box">
+        <span class="close-button" @click="closeLoginOverlay">&times;</span>
+        <h2>Login</h2>
+        <form>
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username"><br><br>
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password"><br><br>
+          <button type="button">Submit</button>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      closeLoginOverlay() {
+        this.$emit('close');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Styling for the login overlay */
+  .overlay {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    justify-content: center;
+    align-items: center;
+  }
+  /* Add your other styles here */
+  </style>
+  
