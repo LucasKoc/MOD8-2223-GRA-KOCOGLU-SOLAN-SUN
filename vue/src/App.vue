1a58cbd3c18d15e5f38e9d663ce313fc0fa5bfd9
Tao Solan <tao.solan@gmail.com>
2023-09-28 00:35:55 -0400

    Changing method in accordance to class

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,54 +1,170 @@
-<script>
-import LoginOverlay from './components/LoginOverlay.vue';
+<script setup>
 
-export default {
-  data() {
-    return {
-      showLoginOverlay: false,
-    };
-  },
-  components: {
-    LoginOverlay,
-  },
-  methods: {
-    openLoginOverlay() {
-      this.showLoginOverlay = true;
-    },
-    closeLoginOverlay() {
-      this.showLoginOverlay = false;
-    },
-  },
-};
+  //import {ref} from 'vue';
+  import HomePage from './components/HomePage.vue';
+
 </script>
 
 <template>
-  <div id="app">
-    <div class="container">
-      <!-- Fancy homepage content -->
-      <h1>Welcome to Our Website</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit....</p>
-      <button @click="openLoginOverlay">Login</button>
-    </div>
+  <header>
+      <a href="index.html">
+          <img id="logo" src="picture/icon.png" alt="Logo" width="50" height="50">
+      </a>
+      <nav>
+          <ul id="menuclick">
+              <li>
+                  <a href="article.html">Article</a>
+              </li>
+              <li>
+                  <a href="login.html">Login</a>
+              </li>
+          </ul>
+      </nav>
+  </header>
 
-    <!-- Login overlay -->
-    <LoginOverlay v-if="showLoginOverlay" @close="closeLoginOverlay" />
-  </div>
+  <main>
+    
+    <HomePage />
+
+  </main>
+
+  <footer>
+      <p id="footertext">All right reserved</p>
+  </footer>
+
 </template>
 
 <style>
 
-.container {
-    max-width: 1200px;
-    margin: 0 auto;
+  body{
+    background-color: white;
+    color: black;
+    font-family: 'Times New Roman', Times, serif;
+    width: 100%;
+    margin: 0px;
+    margin-top: 80px;
+  }
+
+  header {
+    background-color: rgb(227, 255, 190);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+
+  footer {
+    background-color: rgb(227, 255, 190);
+    color: black;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  main{
+    margin-left: 10%;
+    margin-right: 10%;
+  }
+
+  article{
+    margin-top: 2%;
+    margin-bottom: 5%;
+  }
+
+  article h1{
+    margin-left: 1%;
+  }
+
+  set {
+    display: flex;
+    flex-wrap: nowrap;
+  }
+
+  set #bottom{
+    align-self: flex-end;
+  }
+
+  img {
+    margin: 1%;
+    display: block;
+    max-width:300px;
+    max-height:200px;
+    width: auto;
+    height: auto;
+  }
+
+  #menuclick {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+  }
+
+  #menuclick li {
+    margin-right: 10px;
+  }
+
+  #menuclick li:last-child {
+    margin-right: 0;
+  }
+
+  #menuclick a {
+    text-decoration: none;
+    padding: 10px;
+    border: 1px solid black;
+    color: black;
+    transition: color 0.2s, background-color 0.2s;
+  }
+
+  #menuclick a:hover {
+    color: red;
+    background-color: black;
+  }
+
+  #loginTable table td, #staticTable table td{
+    border: 3px solid black;
+    padding: 6px;
+  }
+
+  #logo {
+    margin-right: 10px; 
+  }
+
+  #sidenav {
+    position: fixed;
+    left: 5px;
+    top: 80px;
+    width: 200px;
     padding: 20px;
   }
 
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  #sidenav ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  #sidenav li {
+    margin-bottom: 5px;
+  }
+
+  #sidenav a {
+    text-decoration: none;
+    color: black;
+  }
+
+  #staticTable  tr{
+    padding: 10%;
+  }
+
+  #staticTable table{
+    width: 50%;
   }
 </style>
--- vue/src/components/LoginOverlay.vue
+++ vue/src/components/LoginOverlay.vue
@@ -1,42 +1,47 @@
+<script setup>
+
+    import { ref } from 'vue';
+
+    const tes = ref("yey")
+
+    const emit = defineEmits(['close'])
+
+    function closeLoginOverlay() {
+        emit('close');
+    }
+
+
+</script>
+
 <template>
-    <div class="overlay">
-      <div class="login-box">
-        <span class="close-button" @click="closeLoginOverlay">&times;</span>
+
+    <panel>
+        <div class="login-box">
         <h2>Login</h2>
         <form>
-          <label for="username">Username:</label>
-          <input type="text" id="username" name="username"><br><br>
-          <label for="password">Password:</label>
-          <input type="password" id="password" name="password"><br><br>
-          <button type="button">Submit</button>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username"><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password"><br><br>
+            <button type="button" @click="closeLoginOverlay">{{tes}}</button>
         </form>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      closeLoginOverlay() {
-        this.$emit('close');
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  /* Styling for the login overlay */
-  .overlay {
-    display: none;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.7);
-    justify-content: center;
-    align-items: center;
-  }
-  /* Add your other styles here */
-  </style>
+        </div>
+    </panel>
+
+</template>
+
+<style scoped>
+    /*overlay*/
+    panel {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0,0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
   
