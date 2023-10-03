b5c9d1eca019d81fcd8501f855420258c6ab5fd7
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-10-02 22:17:26 -0400

    Created Page RoomPageView for /rooms and components

---
+++ vue-project/src/components/RoomThumbnail.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="room-thumbnail" @click="$emit('click')">
+    <span class="room-title">{{ room["room-name"] }}</span>
+    <span :class="'availability ' + availabilityClass()">{{ room.availability }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RoomThumbnail",
+  props: {
+    room: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    availabilityClass() {
+      if (this.room.availability === 'Available') {
+        return 'available';
+      } else if (this.room.availability === 'Unavailable') {
+        return 'unavailable';
+      } else if (this.room.availability.includes('Available in')) {
+        return 'available-in';
+      }
+      return '';
+    },
+  }
+}
+</script>
+
+<style scoped>
+.room-thumbnail {
+  width: 150px;
+  height: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  border: 1px solid black;
+  cursor: pointer;
+}
+
+.room-title {
+  z-index: 1;
+}
+
+.availability {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+
+  text-align: right;
+  font-size: 75%;
+}
+
+/* Different color coding based on availability. Adjust colors as you see fit. */
+.availability.available {
+  color: green;
+}
+
+.availability.unavailable {
+  color: red;
+}
+
+.availability.available-in {
+  color: orange;
+}
+</style>
