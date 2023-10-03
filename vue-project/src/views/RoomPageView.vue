eae6e590f241d21d4f3deb552028a5d85986b1d8
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-10-03 15:07:29 -0400

    fix css merge

--- vue-project/src/views/RoomPageView.vue
+++ vue-project/src/views/RoomPageView.vue
@@ -1,12 +1,14 @@
 <template>
-  <main>
-    <div v-for="(rooms, type) in typeOfRooms" :key="type">
-      <h1 class="roomTypeTitle">{{ type.split('-')[0].charAt(0).toUpperCase() + type.split('-')[0].slice(1) }} Rooms:</h1>
-      <div class="book-container">
-        <div v-for="room in rooms" :key="room.id">
-          <RoomThumbnail :room="room"/>
-        </div>
+  <main id="rooms-status-main">
+    <div id="rooms-status-div">
+      <div v-for="(rooms, type) in typeOfRooms" :key="type" class="rooms-row-container">
+        <h1 class="roomTypeTitle">{{ type.split('-')[0].charAt(0).toUpperCase() + type.split('-')[0].slice(1) }} Rooms:</h1>
+        <div class="room-box-container">
+          <div v-for="room in rooms" :key="room.id">
+            <RoomThumbnail :room="room"/>
+          </div>
       </div>
+    </div>
     </div>
   </main>
 </template>
