29a52c3dd233a78ec5e50b567e1ba4d33266486c
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-10-03 16:54:07 -0400

    RoomPageView and RoomThumbnail css

--- vue-project/src/services/room.js
+++ vue-project/src/services/room.js
@@ -17,6 +17,31 @@ function getRoom (id) {
 
 addRoom({roomname: "Kitchen 1", roomtype: "kitchen", reservation: [{date: "2021-05-01", time: "12:00", userid: 1}, {date: "2021-05-02", time: "18:00", userid: 1}]});
 addRoom({roomname: "Kitchen 2", roomtype: "kitchen", reservation: [{date: "2021-06-01", time: "18:00", userid: 2}]});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: [], availability: "Available"});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: [], availability: "Unavailable"});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: [], availability: "Available in 30min"});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
+addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
 addRoom({roomname: "Theater Room", roomtype: "multimedia", reservation: []});
 
 export default function roomData() {
