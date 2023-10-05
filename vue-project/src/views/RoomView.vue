4757c825e85f4ff71c2a390071c7b161dfe842f9
jeanjacquessun <104087705+jeanjacquessun@users.noreply.github.com>
2023-10-04 23:39:05 -0400

    css RoomView + css Admin

--- vue-project/src/views/RoomView.vue
+++ vue-project/src/views/RoomView.vue
@@ -36,11 +36,13 @@
     <div class="reservation-view">
         <div class="flexdivtop">
             <h2>{{roomname}}</h2>
-            <button class="reservebutton" @click="openModal()">Reserve</button>
         </div>
         <div class="flexdiv">
             <div>
+              <div class="title">
                 <h3>Room Reservations</h3>
+              <button class="reservebutton" @click="openModal()">Reserve</button>
+                </div>
                 <div class="reservation">
                     <ul>
                         <li v-for="roomReservation in roomReservations" :key="roomReservation.id">
@@ -55,85 +57,8 @@
     <div class="backdrop" v-if="roomManage"></div>
     <RoomRes class="user-modal" v-if="roomManage" @sendData="(data) => saveData(data)"/>
 </template>
-  
-<style scoped>
-    .reservation-view {
-        padding: 20px 0px;
-        background-color: white;
-        color: black;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    }
 
-    .flexdiv {
-        margin: 0% 5%;
-    }
 
-    .flexdivtop {
-        display: flex;
-        margin: 0% 5%;
-        align-items: baseline;
-        justify-content: space-around;
-    }
-
-    .reservation {
-        padding: 1%;
-        box-shadow: 0px 0px 1px 1px rgba(0,0,0,0.1);
-    }
-
-    .reservebutton {
-        padding: 10px;
-        background-color: #007BFF;
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        font-size: 16px;
-    }
-
-    .reservebutton:hover {
-        background-color: #0056b3;
-    }
-    
-    h2 {
-        margin: 2% 5%;
-    }
-    
-    h3 {
-        margin-top: 10px;
-    }
-    
-    ul {
-        list-style: none;
-        padding: 0;
-    }
-    
-    li {
-        margin-top: 10px;
-        padding: 10px;
-        background-color: #fff;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        display: flex;
-        justify-content: space-between;
-    }
-    
-    p {
-        background-color: #fff;
-        padding: 10px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        text-align: center;
-    }
-    
-    p.v-if {
-        background-color: #007BFF;
-        color: white;
-    }
-</style>
 
 <style scoped>
 @import '../assets/css/views/RoomView.css';
