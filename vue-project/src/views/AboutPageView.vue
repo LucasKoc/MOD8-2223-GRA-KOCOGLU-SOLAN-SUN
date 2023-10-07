7833121622b9716161ea915996b71e2855953633
Tao Solan <tao.solan@gmail.com>
2023-10-06 21:46:51 -0400

    Making about

--- vue-project/src/views/AboutPageView.vue
+++ vue-project/src/views/AboutPageView.vue
@@ -1,22 +1,29 @@
 <template>
   <main>
     <div class="about">
+      <h1>Site made by</h1>
       <article>
-        <h1>Reservation</h1>
+        <h1>KOCOGLU Lucas</h1>
         <p>
-          Reservation is a Vue.js application that allows users to make reservations for a
-          restaurant.
+          QUOI.
         </p>
-        <p>It is built with Vue.js, Vue Router, and Tailwind CSS.</p>
+        <p><a href="https://www.linkedin.com/in/lucas-kocoglu/">Linkedin</a></p>
       </article>
 
       <article>
-        <h2>Features</h2>
-        <ul>
-          <li>Users can make reservations for a restaurant.</li>
-          <li>Users can view their reservations.</li>
-          <li>Users can delete their reservations.</li>
-        </ul>
+        <h1>SUN Jeanjacques</h1>
+        <p>
+          FEUR.
+        </p>
+        <p><a href="https://www.linkedin.com/in/jean-jacques-sun-268219222/">Linkedin</a></p>
+      </article>
+
+      <article>
+        <h1>SOLAN Tao</h1>
+        <p>
+          Student in computer science at Efrei Paris, like space bit too much.
+        </p>
+        <p><a href="https://www.linkedin.com/in/tao-solan-a3b5b421b/">Linkedin</a></p>
       </article>
     </div>
   </main>
