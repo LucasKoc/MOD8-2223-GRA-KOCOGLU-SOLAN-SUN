8c98300efa6ebb9df99d5f3a759a4fa0c12e6ce8
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-10-18 19:46:50 -0400

    Update user.js

--- vue-project/api/server/routes/v1/user.js
+++ vue-project/api/server/routes/v1/user.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import user from '../../persistence/users-repository.js'
 import validator from "../../validators/user-validator.js";
-import room from "../../persistence/room-repository.js";
+
 
 
 const router = express.Router()
@@ -26,7 +26,7 @@ router.get('/users/:userId', async (req, res,next) => {
 })
 router.post('/users', async(req,res,next)=>{
     const userId = req.body.room
-    const code = req.body.keyp
+    const code = req.body.key
     const name = req.body.name
 
     const err = await validator.validateUserIdentity(userId,code,name);
@@ -43,25 +43,159 @@ router.post('/users', async(req,res,next)=>{
         res.status(404).json({error: 'Reservation could not be created.'});
 
     }
-    // res.status(201).json(await user.addUser(req.body))
 })
-router.delete('/users/:id', async (req, res) => { res.status(200).json(await user.deleteUser(req.params.id)) })
-router.patch('/users/:id', async (req, res) => {res.status(202).json(await user.modifyUser(req.params.id, req.body)) })
-router.get('/users/key/:key', async (req, res) => {res.status(200).json(await user.findUserByKey(req.params.key)) })
+router.delete('/users/:userId', async (req, res,next) => {
 
-router.put('/users/:id/ban/:bantime', async (req, res) => {
+    const userID = Number.parseInt(req.params.userId)
+    const err = validator.validateDeleteUser(userID)
+    if (err) {
+        res.status(400).json({error: err});
+        return next(err)
+    }
+
+    const del = await user.deleteUser(req.params.userId);
+    if (del) {
+        res.status(202).json({message: 'User has been successfully deleted.'});
+    } else {
+        res.status(404).json({error: 'User does not exist or could not be deleted.'});
+    }
+
+
+})
+router.patch('/users/:userId', async (req, res,next) => {
+    const userID = Number.parseInt(req.params.userId)
+    const personalKey = req.body.key
+    const name = req.body.name
+    const err = validator.validateModifyUser(userID,name,personalKey)
+    if (err) {
+        res.status(400).json({error: err});
+        return next(err)
+    }
+    const change = await user.modifyUser(req.params.userId, req.body);
+    if (!change) {
+        res.status(202).json({message: 'User has been successfully modified.'});
+    } else {
+        res.status(404).json({error: 'User could not be modified.'});
+    }
+
+    // res.status(202).json(await user.modifyUser(req.params.userId, req.body))
+})
+router.get('/users/key/:key', async (req, res,next) => {
+    const key = req.params.key
+    const err = validator.validateKey(key)
+    if (err) {
+        res.status(400).json({error: err});
+        return next(err)
+    }
+    const find = await user.findUserByKey(req.params.key)
+    if (find) {
+        res.status(200).json(await user.findUserByKey(req.params.key));
+    }
+    else
+    {
+        res.status(404).json({error: 'key does not exist.'});
+    }
+    // res.status(200).json(await user.findUserByKey(req.params.key))
+})
+
+router.put('/users/:userId/ban/:bantime', async (req, res,next) => {
+
+    const userID = Number.parseInt(req.params.userId)
+    const banT = Number.parseInt(req.params.bantime)
+    const errBanTime = validator.validateBanTime(banT)
+    if (errBanTime) {
+        res.status(400).json({error: errBanTime});
+        return next(errBanTime)
+    }
+    const userIdError = await validator.validateBannedID(userID);
+    if (userIdError) {
+        res.status(400).json({ error: userIdError.message });
+        return next(userIdError);
+    }
+
     const banTime = new Date().getTime() + req.params.bantime * 60 * 1000 * 60 *24
-    res.status(200).json(await user.banUser(banTime, req.params.id)) 
-})
-router.get('/users/:id/ban', async (req, res) => {res.status(200).json(await user.isUserBanned(req.params.id)) })
 
-router.get('/users/verify', async (req, res) => {res.status(200).json(await user.verifyUser(req.body)) })
-router.post('/users/login', async (req, res) => {
-    res.status(200).json(await user.login(req.body, req))
+
+
+    res.status(200).json(await user.banUser(banTime, req.params.userId))
 })
-router.post('/users/:id/logout', async (req, res) => {
-    res.status(200).json(await user.logout(req.params.id, req))
+router.get('/users/:userId/ban', async (req, res,next) => {
+    const userID = Number.parseInt(req.params.userId)
+    const err = validator.validateUser(userID)
+    if (err) {
+        res.status(400).json({error: err});
+        return next(err)
+    }
+    const find = await user.isUserBanned(req.params.userId)
+    if(find)
+    {
+        res.status(200).json(await user.isUserBanned(req.params.userId))
+    }
+    else
+    {
+        res.status(404).json({error: 'user ban does not exist.'});
+    }
+
 })
-router.get('/users/:id/connected', async (req, res) => {res.status(200).json(await user.getConnectedUser(req.params.id)) })
+
+// Deprecated
+router.get('/users/verify', async (req, res) => {
+    res.status(200).json(await user.verifyUser(req.body))
+})
+
+router.post('/users/login', async (req, res,next) => {
+    const roomNumber = req.body.room
+    const code = req.body.key
+    const name = req.body.name
+
+    const err = await validator.validateUserLogin(roomNumber, code, name);
+
+    if (err) {
+        res.status(400).json({error: err});
+        return next(err)
+    }
+    const log = (await user.login(req.body, req))
+    if (log) {
+        res.status(201).json({message: 'You are login.'})
+    } else {
+        res.status(404).json({error: "Login operation can't be established maybe you don't already have a account."});    // res.status(200).json(await user.login(req.body, req))
+    }
+
+})
+router.post('/users/:userId/logout', async (req, res,next) => {
+    const userID = Number.parseInt(req.params.userId)
+    const err = validator.validateUserLogout(userID)
+    if (err) {
+        res.status(400).json({error: err});
+        return next(err)
+    }
+    const log = (await user.logout(req.params.userId, req))
+    if (log) {
+        res.status(201).json({message: 'You are logout.'})
+    }
+    else
+    {
+        res.status(404).json({error: "Logout operation can't be established maybe because you are not login."});
+    }
+
+})
+router.get('/users/:userId/connected', async (req, res,next) => {
+    const userID = Number.parseInt(req.params.userId)
+    const err = validator.validateUserConnection(userID)
+    if (err) {
+        res.status(400).json({error: err});
+        return next(err)
+    }
+    const find = await user.getConnectedUser(req.params.userId)
+    if(find)
+    {
+        res.status(200).json(await user.getConnectedUser(req.params.userId))
+    }
+    else
+    {
+        res.status(404).json({error: 'user does not exist.'});
+    }
+
+})
 
 export default router
